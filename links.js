af821a826010b8a2c8235c4e5b22607b9e8840fd
H <juana.galan@outlook.com>
2020-02-13 14:51:24 +0000

    Loops through URLs

--- links.js
+++ links.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
-const xml2js = require("xml2js");
+const DomParser = require("dom-parser");
+const parser = new DomParser();
 const puppeteer = require("puppeteer");
 
 (async () => {
@@ -7,16 +8,21 @@ const puppeteer = require("puppeteer");
     const resp = await fetch("https://www.lickhome.com/sitemap.xml");
     console.log("Fetches sitemap");
     const xml = await resp.text();
-    const parser = new xml2js.Parser();
-    const doc = parser.parseString(xml, "text/xml");
+    const doc = parser.parseFromString(xml, "text/xml");
+    console.log("Parses XML");
     const urls = [...doc.getElementsByTagName("loc")].map(
       loc => loc.textContent
     );
+    console.log("Creates array of URLs");
     const browser = await puppeteer.launch({ headless: false });
     console.log("Launches puppeteer");
     const page = await browser.newPage();
-    await page.goto(urls);
-    console.log("Gets URLs");
+    // Loop through URLs as individual strings rather than as an array
+    for (i = 0; i < urls.length; i++) {
+      // do whatever with urls[i]
+      await page.goto(urls[i]);
+      console.log("Gets URLs");
+    }
     // Wait until <meta property="og:title"> has a truthy value for content attribute
     await page.waitForFunction(() => {
       return document
